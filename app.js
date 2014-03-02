4dbeb7d7a35c85613cfd7c9818ae73b27ddd63ef
cho45 <cho45@lowreal.net>
2014-03-02 23:57:05 +0900

    canvas

--- app.js
+++ app.js
@@ -1,6 +1,80 @@
 
 var App = angular.module('App', []);
 
+
+App.directive('pinheader', function ($timeout, $parse) {
+	var margin = 10;
+	var targetDPI = 350;
+	var nameWidth = 3;
+	var pinNumberWidth = 1;
+	var pinPlaceHolder = 1;
+
+	function redraw (canvas, val) {
+		var wholeWidth = (nameWidth + pinNumberWidth + 1) * 2;
+		var wholeHeight = val.length + 2;
+		console.log([wholeWidth, wholeHeight]);
+
+		canvas.width  = wholeWidth  * (targetDPI / 10) + (margin * 2);
+		canvas.height = wholeHeight * (targetDPI / 10) + (margin * 2);
+		canvas.style.width  = canvas.width  / 2 + 'px';
+		canvas.style.height = canvas.height / 2 + 'px';
+
+		var ctx = canvas.getContext('2d');
+		ctx.save();
+		ctx.translate(margin, margin);
+		// swtich scale to 100 mil (100mil = 0.1in)
+		ctx.scale(
+			targetDPI / 10,
+			targetDPI / 10
+		);
+		console.log(targetDPI / 10);
+
+		// draw outline
+		ctx.lineWidth = 0.1;
+		ctx.beginPath();
+		ctx.moveTo(0, 0);
+		ctx.lineTo(wholeWidth, 0);
+		ctx.lineTo(wholeWidth, wholeHeight-1);
+		ctx.lineTo(0, wholeHeight-1);
+		ctx.closePath();
+		ctx.stroke();
+
+		// vertical lines
+		var x = 0;
+		ctx.beginPath();
+		ctx.moveTo(x += nameWidth, 1); ctx.lineTo(x, wholeHeight - 2);
+		ctx.moveTo(x += pinNumberWidth, 1); ctx.lineTo(x, wholeHeight - 2);
+		ctx.moveTo(x += pinPlaceHolder, 1); ctx.lineTo(x, wholeHeight - 2);
+		ctx.moveTo(x += pinPlaceHolder, 1); ctx.lineTo(x, wholeHeight - 2);
+		ctx.moveTo(x += pinNumberWidth, 1); ctx.lineTo(x, wholeHeight - 2);
+		ctx.stroke();
+
+		// horizontal lines
+		for (var i = 0, len = wholeHeight; i < len; i++) {
+			ctx.beginPath();
+			ctx.moveTo(0, i);
+			ctx.lineTo(wholeWidth, i);
+			ctx.stroke();
+		}
+
+		ctx.font = "0.1px Arial";
+		for (var i = 0, len = val.length; i < len; i++) {
+			ctx.fillText(i, nameWidth + 0.1, i);
+		}
+
+		ctx.restore();
+	}
+
+	return {
+		link : function (scope, element, attrs) {
+			var model = $parse(attrs.pinheader);
+			scope.$watch(model, function (val) {
+				redraw(element[0], val);
+			});
+		}
+	};
+});
+
 App.controller('MainCtrl', function ($scope) {
 
 	$scope.update = function () {
@@ -38,7 +112,7 @@ App.controller('MainCtrl', function ($scope) {
 			rows.reverse();
 		}
 
-		$scope.rows = rows;
+		$scope.main = rows;
 	};
 
 	$scope.update();
